5612d72f3f9f4a2da016fccca432ff4f3a5bfff5
imran61999 <smemrannit@gmail.com>
2024-04-10 14:02:22 +0600

    added condition and login validation

--- src/Compunent/Register/Register.jsx
+++ src/Compunent/Register/Register.jsx
@@ -1,7 +1,7 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebese/firebase.config";
 import { useState } from "react";
-import { FaEye , FaEyeSlash } from 'react-icons/fa';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 
 const Login = () => {
@@ -9,12 +9,13 @@ const Login = () => {
     const [registerError, setRegisterError] = useState('');
     const [success, setSuccess] = useState('');
     // password show or unshow
-    const [showPassword , setShowPassword] =useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleLogin = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const accepted = e.target.terms.required;
         console.log(email, password);
 
 
@@ -23,8 +24,12 @@ const Login = () => {
             setRegisterError(" Password should be at least 6 characters");
             return;
         }
-        else if (!/[A-Z]/.test(password)){
+        else if (!/[A-Z]/.test(password)) {
             setRegisterError('Password must have at least one Uppercase Character.');
+            return;
+        }
+        else if(!accepted){
+            setRegisterError("please accepted our terms and conditions!");
             return;
         }
 
@@ -52,15 +57,21 @@ const Login = () => {
             <label className="text-3xl">Please Register Now</label>
             <form onSubmit={handleLogin}>
 
-                <input className="border mb-3 w-3/4" type="email" name="email" placeholder="Enter your email" required />
+                <input className="border w-full mb-3" type="email" name="email" placeholder="Enter your email" required />
                 <br />
-                <input className="border mb-3 w-3/4" type={showPassword ? "text" : "password"} name="password" placeholder="Enter your password" required />
-                <span onClick={ () => setShowPassword (!showPassword)}>
+                <div className="mb-4 relative">
+                    <input className="border w-full mb-3" type={showPassword ? "text" : "password"} name="password" placeholder="Enter your password" required />
+                    <span className="absolute top-2 right-2" onClick={() => setShowPassword(!showPassword)}>
 
-                    {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
-                </span>
+                        {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+                    </span>
+                </div>
                 <br />
-                <button className="btn  btn-secondary mb-3 w-3/4">Register</button>
+                <div className="mb-4">
+                    <input type="checkbox" name="terms" id="terms"/>
+                    <label className="ml-2" htmlFor="terms">Accept our <a href="">terms and condition</a></label>
+                </div>
+                <button className="btn  btn-secondary mb-3 w-full">Register</button>
             </form>
             {
                 registerError && <p className="text-red-600">{registerError}</p>
